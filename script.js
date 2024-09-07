74c3136ddb7d7296717e02ec5ae16da024588d0c
samar-58 <sayedsamar58@gmail.com>
2024-09-07 16:48:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,21 +23,16 @@ const swiper = new Swiper('.card-slider', {
 });
 
     function updateMiddleSlide(swiper) {
-        // Remove the 'middle' class from all slides
         document.querySelectorAll('.swiper-slide').forEach(slide => {
             slide.classList.remove('middle');
         });
     
-        // Get the active slide index
         const activeIndex = swiper.activeIndex;
     
-        // Get the slides
         const slides = document.querySelectorAll('.swiper-slide');
         const slidesPerView = swiper.params.slidesPerView;
     
-        // Determine the middle index
         const middleIndex = Math.floor(slidesPerView / 2);
     
-        // Add 'middle' class to the slide that should be in the middle
         slides[(activeIndex + middleIndex) % slides.length].classList.add('middle');
     }
